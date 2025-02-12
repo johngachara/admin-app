e0c2b45461fdf2801a2353b494718d0189f0aded
johngachara <johngachara29@gmail.com>
2025-02-12 20:09:22 +0300

    add vercel speed insights component

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { SpeedInsights } from "@vercel/speed-insights/react"
 if (!PUBLISHABLE_KEY) {
     throw new Error('Add your Clerk Publishable Key to the .env file')
 }
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+      <SpeedInsights/>
       <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <App />
       </ClerkProvider>
